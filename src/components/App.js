5fa2df5a8d9508a1517787f6ad61984eb9fedaa9
Edward White <ewhite91186@gmail.com>
2018-06-01 19:02:37 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Register from './Register';
+import Login from './Login';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route exact path="/" render={() => (<Home />)} />
+					<Route path="/register" render={() => (<Register />)} />
+					<Route path="/login" render={() => (<Login />)} />
+				</Switch>
+			</div>
+		)
+	}
+}
+
+export default App;
