a134531d899921ef441db84f5aac6d87548aaa2a
Edward White <ewhite91186@gmail.com>
2018-06-01 19:19:04 -0500

    added links to login logout register

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			user: false
+		}
+	}
+
+	renderLinks() {
+		if (this.state.user) {
+			return (
+				<ul>
+					<li>Home</li>
+					<li>Logout</li>
+				</ul>
+			)
+		} else {
+			return (
+				<ul>
+					<Link to="/"><li>Home</li></Link>
+					<Link to="/login"><li>Login</li></Link>
+					<Link to="/register"><li>Register</li></Link>
+				</ul>
+			)
+		}
+	}
+
 	render() {
 		return (
-			<div>Home</div>
+			<div>
+				{this.renderLinks()}
+				<div>Home</div>
+			</div>
 		)
 	}
 }
