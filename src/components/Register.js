f275077eb8ab3387af97f2fb7c935bfefc701155
Edward White <ewhite91186@gmail.com>
2018-06-05 17:03:56 -0500

    register component renders a form

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,35 @@ import React from 'react';
 class Register extends React.Component {
 	render() {
 		return (
-			<div>Register</div>
+			<div>
+				<div className="page-header">
+					<h1>User Registration</h1>
+				</div>
+
+				<form>
+					<div>
+						<label>Username:</label>
+						<input type="text" name="name" placeholder="please enter username" required />
+					</div>
+					<br />
+					<div>
+						<label>Email:</label>
+						<input type="text" name="email" placeholder="please enter email" required />
+					</div>
+					<br />
+					<div>
+						<label>Password:</label>
+						<input type="password" name="firstPassword" placeholder="please enter password" required />
+					</div>
+					<br />
+					<div>
+						<label>Confirm Password:</label>
+						<input type="password" name="confirmPassword" placeholder="please enter password" required />
+					</div>
+					<br />
+					<button type="submit">Register</button>
+				</form>
+			</div>
 		)
 	}
 }
