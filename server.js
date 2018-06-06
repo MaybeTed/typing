142e97ddf6851614c9a92e40d3b19bfcd36ec04a
Edward White <ewhite91186@gmail.com>
2018-06-06 17:07:00 -0500

    connection to db

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const router = express.Router();
+const appRoutes = require('./api')(router);
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/dist')));
+app.use('/api', appRoutes);
+
+
+mongoose.connect('mongodb://localhost:27017/typing', (err) => {
+	if (err) {
+		console.log('Not connected to the database: ', err);
+	} else {
+		console.log('successfully connected to MongoDB');
+	}
+});
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, './dist/index.html'));
@@ -12,5 +28,5 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
 	console.log('Server is listening on port: ', port);
-})
+});
 
