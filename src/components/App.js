deba51b7f970770ff4729ad6a5a3518dfe08c584
Edward White <ewhite91186@gmail.com>
2018-06-07 13:09:24 -0500

    import Nav

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
+import Nav from './Nav';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<Nav />
 				<Switch>
 					<Route exact path="/" render={() => (<Home />)} />
 					<Route path="/register" render={() => (<Register />)} />
