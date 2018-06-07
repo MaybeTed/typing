74163a3bf5da2f242008c47b0154a0cffb1ff31f
Edward White <ewhite91186@gmail.com>
2018-06-07 13:14:07 -0500

    redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'babel-polyfill';
 import App from './components/App';
+import store from '../store';
+import { Provider } from 'react-redux';
 
 render(
-	<BrowserRouter>
-	  <App />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+		  <App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux';
+import authReducer from './auth_reducer';
+
+const reducer = combineReducers({
+	auth: authReducer,
+});
+
+export default reducer;
